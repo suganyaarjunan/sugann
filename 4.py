d4e6821b07d0cb08758e46649753150d66551c57
suganyaarjunan <52317510+suganyaarjunan@users.noreply.github.com>
2019-07-02 12:00:58 +0530

    Rename 3.py to 4.py

---
+++ 4.py
@@ -0,0 +1,7 @@
+s=input("")
+if s=='a'or n=='e'or n=='i'or n=='o'or n=='u'or n=='A'or n=='E'or n=='I'or n=='O'or n=='U':
+   print("Vowel")
+elif s=='z'or n=='b'or n=='c'or n=='f'or n=='g'or n=='Z'or n=='B'or n=='C'or n=='F'or n=='G':   
+   print("Consonant")
+else:
+   print("invalid")
